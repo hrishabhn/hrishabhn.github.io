902f1f6969056d7cb430b661ce27d39697912c72
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2021-06-22 12:30:01 +0800

    Update date.js

--- script/date.js
+++ script/date.js
@@ -7,7 +7,7 @@ function date(){
     // var weekDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 
     var weekDays = ["SUN","MON","TUE","WED","THU","FRI","SAT"]
-    var months = ["JAN","FEB","MAR","APR","MAY","JUN","JUL"]
+    // var months = ["JAN","FEB","MAR","APR","MAY","JUN","JUL"]
 
     dayNow = fullDate.getDay()
     dayNow = weekDays[dayNow]
@@ -21,4 +21,17 @@ function date(){
     document.getElementById("date-text").innerHTML = dayNow
     // document.getElementById("date-month").innerHTML = monthNow
     document.getElementById("date-number").innerHTML = dateNow
+
+    position = getCurrentPosition()
+    latitude = position.coords.latitude
+    // latitude = "300"
+
+
+    document.getElementById("test").innerHTML = latitude
 }
+
+// let apiKey = '****************************';
+// let city = 'portland';
+// let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+// api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
