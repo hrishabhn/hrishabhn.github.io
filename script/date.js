37aa81262b9465b873b7e6d824cda34a224a18a6
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2021-06-22 11:54:45 +0800

    Update date.js

--- script/date.js
+++ script/date.js
@@ -4,19 +4,21 @@
 function date(){
     var fullDate = new Date();
 
-    var weekDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    // var weekDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+    var weekDays = ["SUN","MON","TUE","WED","THU","FRI","SAT"]
     var months = ["JAN","FEB","MAR","APR","MAY","JUN","JUL"]
 
     dayNow = fullDate.getDay()
     dayNow = weekDays[dayNow]
 
-    monthNow = fullDate.getMonth()
-    monthNow = months[monthNow]
+    // monthNow = fullDate.getMonth()
+    // monthNow = months[monthNow]
 
     dateNow = fullDate.getDate()
 
     
-    // document.getElementById("day").innerHTML = dayNow
-    document.getElementById("date-month").innerHTML = monthNow
+    document.getElementById("date-text").innerHTML = dayNow
+    // document.getElementById("date-month").innerHTML = monthNow
     document.getElementById("date-number").innerHTML = dateNow
 }
