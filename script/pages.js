12753d3fb8f606ed1717465cdec288793fe83d31
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2021-06-27 20:18:05 +0800

    Update pages.js

--- script/pages.js
+++ script/pages.js
@@ -3,24 +3,28 @@ var testing = document.getElementById("main-2");
 
 
 function mainPage1(){
+  document.getElementById("menu-1").className = "menu-item selected";
+  document.getElementById("menu-2").className = "menu-item";
+  
   document.getElementById("title-text").innerHTML = "Home";
   document.getElementById("title-page").innerHTML = "Home";
 
-  document.getElementById("main-1").className = "main";
-  document.getElementById("main-2").className = "main-right main";
+  document.getElementById("wallpaper").className = "wallpaper wp-1"
 
-  document.getElementById("menu-1").className = "menu-item selected";
-  document.getElementById("menu-2").className = "menu-item";
+  document.getElementById("main-1").className = "main";
+  document.getElementById("main-2").className = "main-right main";  
 }
 
 function mainPage2(){
+  document.getElementById("menu-1").className = "menu-item";
+  document.getElementById("menu-2").className = "menu-item selected";
+
   document.getElementById("title-text").innerHTML = "Media";
   document.getElementById("title-page").innerHTML = "Media";
 
+  document.getElementById("wallpaper").className = "wallpaper wp-2"
+
   document.getElementById("main-1").className = "main-left main";
   document.getElementById("main-2").className = "main";
-
-  document.getElementById("menu-1").className = "menu-item";
-  document.getElementById("menu-2").className = "menu-item selected";
 }
 
