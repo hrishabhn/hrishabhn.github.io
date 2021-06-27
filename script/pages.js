3f94668625be03b7811c61ce6ae80739f2730753
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2021-06-27 21:57:24 +0800

    Update pages.js

--- script/pages.js
+++ script/pages.js
@@ -5,7 +5,9 @@ var testing = document.getElementById("main-2");
 function mainPage1(){
   document.getElementById("menu-1").className = "menu-item selected";
   document.getElementById("menu-2").className = "menu-item";
-  
+
+  document.getElementById("menu-current").className = "menu-current menu-current-1";
+
   document.getElementById("title-text").innerHTML = "Home";
   document.getElementById("title-page").innerHTML = "Home";
 
@@ -18,6 +20,8 @@ function mainPage1(){
 function mainPage2(){
   document.getElementById("menu-1").className = "menu-item";
   document.getElementById("menu-2").className = "menu-item selected";
+
+  document.getElementById("menu-current").className = "menu-current menu-current-2";
 
   document.getElementById("title-text").innerHTML = "Media";
   document.getElementById("title-page").innerHTML = "Media";
