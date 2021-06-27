b84f09f6f87688d3c6d0580a1d56439e62ad330d
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2021-06-28 00:10:14 +0800

    3

--- script/pages.js
+++ script/pages.js
@@ -5,6 +5,7 @@ var testing = document.getElementById("main-2");
 function mainPage1(){
   document.getElementById("menu-1").className = "menu-item selected";
   document.getElementById("menu-2").className = "menu-item";
+  document.getElementById("menu-3").className = "menu-item";
 
   document.getElementById("menu-current").className = "menu-current menu-current-1";
 
@@ -14,21 +15,40 @@ function mainPage1(){
   document.getElementById("wallpaper").className = "wallpaper wp-1"
 
   document.getElementById("main-1").className = "main";
-  document.getElementById("main-2").className = "main-right main";  
+  document.getElementById("main-2").className = "main-right main";
+  document.getElementById("main-3").className = "main-right main";  
 }
 
 function mainPage2(){
   document.getElementById("menu-1").className = "menu-item";
   document.getElementById("menu-2").className = "menu-item selected";
+  document.getElementById("menu-3").className = "menu-item";
 
   document.getElementById("menu-current").className = "menu-current menu-current-2";
 
-  document.getElementById("title-text").innerHTML = "Media";
-  document.getElementById("title-page").innerHTML = "Media";
+  document.getElementById("title-text").innerHTML = "Apps";
+  document.getElementById("title-page").innerHTML = "Apps";
 
   document.getElementById("wallpaper").className = "wallpaper wp-2"
 
   document.getElementById("main-1").className = "main-left main";
   document.getElementById("main-2").className = "main";
+  document.getElementById("main-3").className = "main-right main";
 }
 
+function mainPage3(){
+  document.getElementById("menu-1").className = "menu-item";
+  document.getElementById("menu-2").className = "menu-item";
+  document.getElementById("menu-3").className = "menu-item selected";
+
+  document.getElementById("menu-current").className = "menu-current menu-current-3";
+
+  document.getElementById("title-text").innerHTML = "Media";
+  document.getElementById("title-page").innerHTML = "Media";
+
+  document.getElementById("wallpaper").className = "wallpaper wp-3"
+
+  document.getElementById("main-1").className = "main-left main";
+  document.getElementById("main-2").className = "main-left main";
+  document.getElementById("main-3").className = "main";
+}
