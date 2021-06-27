d0c136c42380a7d78e14e6d375d5e4f184e807fc
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2021-06-27 19:23:52 +0800

    Update pages.js

--- script/pages.js
+++ script/pages.js
@@ -3,11 +3,13 @@ var testing = document.getElementById("main-2");
 
 
 function mainPage1(){
+  document.getElementById("title-text").innerHTML = "Home";
   document.getElementById("main-1").className = "main";
   document.getElementById("main-2").className = "main-right main";
 }
 
 function mainPage2(){
+  document.getElementById("title-text").innerHTML = "Media";
   document.getElementById("main-1").className = "main-left main";
   document.getElementById("main-2").className = "main";
 }
