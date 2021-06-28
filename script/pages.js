32a249467a201abf20ea28d119aa99934d7d48db
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2021-06-28 18:54:53 +0800

    Update pages.js

--- script/pages.js
+++ script/pages.js
@@ -6,7 +6,9 @@ function mainPage1(){
   document.getElementById("menu-1").className = "menu-item selected";
   document.getElementById("menu-2").className = "menu-item";
   document.getElementById("menu-3").className = "menu-item";
-
+  document.getElementById("menu-4").className = "menu-item";
+  document.getElementById("menu-5").className = "menu-item";
+  
   document.getElementById("menu-current").className = "menu-current menu-current-1";
 
   document.getElementById("title-text").innerHTML = "Home";
@@ -16,13 +18,17 @@ function mainPage1(){
 
   document.getElementById("main-1").className = "main";
   document.getElementById("main-2").className = "main-right main";
-  document.getElementById("main-3").className = "main-right main";  
+  document.getElementById("main-3").className = "main-right main";
+  document.getElementById("main-4").className = "main-right main";
+  document.getElementById("main-5").className = "main-right main";
 }
 
 function mainPage2(){
   document.getElementById("menu-1").className = "menu-item";
   document.getElementById("menu-2").className = "menu-item selected";
   document.getElementById("menu-3").className = "menu-item";
+  document.getElementById("menu-4").className = "menu-item";
+  document.getElementById("menu-5").className = "menu-item";
 
   document.getElementById("menu-current").className = "menu-current menu-current-2";
 
@@ -34,12 +40,16 @@ function mainPage2(){
   document.getElementById("main-1").className = "main-left main";
   document.getElementById("main-2").className = "main";
   document.getElementById("main-3").className = "main-right main";
+  document.getElementById("main-4").className = "main-right main";
+  document.getElementById("main-5").className = "main-right main";
 }
 
 function mainPage3(){
   document.getElementById("menu-1").className = "menu-item";
   document.getElementById("menu-2").className = "menu-item";
   document.getElementById("menu-3").className = "menu-item selected";
+  document.getElementById("menu-4").className = "menu-item";
+  document.getElementById("menu-5").className = "menu-item";
 
   document.getElementById("menu-current").className = "menu-current menu-current-3";
 
@@ -51,4 +61,48 @@ function mainPage3(){
   document.getElementById("main-1").className = "main-left main";
   document.getElementById("main-2").className = "main-left main";
   document.getElementById("main-3").className = "main";
+  document.getElementById("main-4").className = "main-right main";
+  document.getElementById("main-5").className = "main-right main";
+}
+
+function mainPage4(){
+  document.getElementById("menu-1").className = "menu-item";
+  document.getElementById("menu-2").className = "menu-item";
+  document.getElementById("menu-3").className = "menu-item";
+  document.getElementById("menu-4").className = "menu-item selected";
+  document.getElementById("menu-5").className = "menu-item";
+
+  document.getElementById("menu-current").className = "menu-current menu-current-4";
+
+  document.getElementById("title-text").innerHTML = "Media";
+  document.getElementById("title-page").innerHTML = "Media";
+
+  document.getElementById("wallpaper").className = "wallpaper wp-4"
+
+  document.getElementById("main-1").className = "main-left main";
+  document.getElementById("main-2").className = "main-left main";
+  document.getElementById("main-3").className = "main-left main";
+  document.getElementById("main-4").className = "main";
+  document.getElementById("main-5").className = "main-right main";
+}
+
+function mainPage5(){
+  document.getElementById("menu-1").className = "menu-item";
+  document.getElementById("menu-2").className = "menu-item";
+  document.getElementById("menu-3").className = "menu-item";
+  document.getElementById("menu-4").className = "menu-item";
+  document.getElementById("menu-5").className = "menu-item selected";
+
+  document.getElementById("menu-current").className = "menu-current menu-current-5";
+
+  document.getElementById("title-text").innerHTML = "Media";
+  document.getElementById("title-page").innerHTML = "Media";
+
+  document.getElementById("wallpaper").className = "wallpaper wp-5"
+
+  document.getElementById("main-1").className = "main-left main";
+  document.getElementById("main-2").className = "main-left main";
+  document.getElementById("main-3").className = "main-left main";
+  document.getElementById("main-4").className = "main-left main";
+  document.getElementById("main-5").className = "main";
 }
