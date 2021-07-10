374a1304e38f1a468932c6cdb3eeccebd64a7e96
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2021-07-10 20:01:01 +0800

    Update date.js

--- script/date.js
+++ script/date.js
@@ -135,11 +135,11 @@ function timeSmartCards(){
     console.log(hourNow);
 
     if ((hourNow > 6) && (hourNow < 10)) {
-        document.getElementById("morning-card-1").className = "smart-card-show content-container";
+        // document.getElementById("morning-card-1").className = "smart-card-show content-container";
         // document.getElementById("morning-card-2").className = "smart-card-show content-container";
         console.log("morning");
     } else{
-        document.getElementById("morning-card-1").className = "smart-card-show content-container";
+        // document.getElementById("morning-card-1").className = "smart-card-show content-container";
         // document.getElementById("morning-card-2").className = "smart-card-hide content-container";
         console.log("not morning");
     }
