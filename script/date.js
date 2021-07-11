453e6a271662085dd6d18b42d35e867b8f0b880b
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2021-07-11 12:50:57 +0800

    Update date.js

--- script/date.js
+++ script/date.js
@@ -94,8 +94,8 @@ function smartCards(){
 
         var lat = position.coords.latitude;
         var lon = position.coords.longitude;
-        console.log(lat)
-        console.log(lon)
+        // console.log(lat)
+        // console.log(lon)
 
         // HOME
         // var lat = 22.262;
@@ -120,27 +120,27 @@ function smartCards(){
             document.getElementById("smarthome-card").className = "smart-card-show content-container hidden-desktop"
             document.getElementById("transport-card").className = "smart-card-hide content-container hidden-desktop"
             district = "Home"
-            console.log("at home")
+            // console.log("at home")
         } else{
             document.getElementById("smarthome-card").className = "smart-card-hide content-container hidden-desktop"
             document.getElementById("transport-card").className = "smart-card-show content-container hidden-desktop"
-            console.log("not at home")
+            // console.log("not at home")
         }
 
         if (work) {
             // document.getElementById("office-card").className = "smart-card-show content-container"
             district = "Office"
-            console.log("at office")
+            // console.log("at office")
         } else{
             // document.getElementById("office-card").className = "smart-card-show content-container"
-            console.log("not at office")
+            // console.log("not at office")
         }
 
         if (ktown) {
             district = "Kennedy Town"
-            console.log("ktown")
+            // console.log("ktown")
         } else{
-            console.log("not at ktown")
+            // console.log("not at ktown")
         }
 
 
@@ -148,37 +148,34 @@ function smartCards(){
 
         if ((afternoon) && (!home)) {
             document.getElementById("food-card").className = "smart-card-show content-container";
-            console.log("afternoon");
+            // console.log("afternoon");
         } else{
             document.getElementById("food-card").className = "smart-card-hide content-container";
-            console.log("not afternoon");
+            // console.log("not afternoon");
         }
         if ((evening) && (!home)) {
             document.getElementById("food-card").className = "smart-card-show content-container";
-            console.log("evening");
+            // console.log("evening");
         } else{
             document.getElementById("food-card").className = "smart-card-hide content-container";
-            console.log("not evening");
+            // console.log("not evening");
         }
         if ((night) && (home)) {
             document.getElementById("sleep-card").className = "smart-card-show content-container hidden-desktop";
-            console.log("night");
+            // console.log("night");
         } else{
             document.getElementById("sleep-card").className = "smart-card-hide content-container hidden-desktop";
-            console.log("not night");
+            // console.log("not night");
         }
 
         // district = "West Lafayette"
         document.getElementById("district").innerHTML = district;
 
-        setTimeout(function(){
-            document.getElementById("status-box").classList = "status-box status-box-show";
-        },500);
-        setTimeout(function(){
-            document.getElementById("status-box").classList = "status-box status-box-hide";
-        },4000);
-        
     }
+
+    
+
+
 
     navigator.geolocation.getCurrentPosition(successCallback);
     
@@ -191,6 +188,17 @@ function smartCards(){
     // x.innerHTML = "Latitude: " + lat + "<br>Longitude: " + lon;
 
     
+}
+
+function statusBar(show,hide){
+    setTimeout(function(){
+        document.getElementById("status-box").classList = "status-box status-box-show";
+        // console.log("showing")
+    },show);
+    setTimeout(function(){
+        document.getElementById("status-box").classList = "status-box status-box-hide";
+        // console.log("hiding")
+    },hide);
 }
 
 // function timeSmartCards(){
