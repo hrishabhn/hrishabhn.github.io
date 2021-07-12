d7728a0b7793486670a707e585bb5410e01fbba9
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2021-07-12 18:34:11 +0800

    Update date.js

--- script/date.js
+++ script/date.js
@@ -97,16 +97,18 @@ function smartCards(){
         // console.log(lat)
         // console.log(lon)
 
-
-        const home = (lat > 22.260) && (lat < 22.264) && (lon > 114.131) && (lon < 114.135);
         const pokFuLam = (lat > 22.252) && (lat < 22.273) && (lon > 114.121) && (lon < 114.135);
-        const work = (lat > 22.3905) && (lat < 22.392) && (lon > 113.9695) && (lon < 113.971);
         const cyberport = (lat > 22.260) && (lat < 22.264) && (lon > 114.127) && (lon < 114.131);
+        const home = (lat > 22.260) && (lat < 22.264) && (lon > 114.131) && (lon < 114.135);
+
+        const tuenMun = (lat > 22.37) && (lat < 22.41) && (lon > 113.96) && (lon < 113.985);
+        const work = (lat > 22.3905) && (lat < 22.392) && (lon > 113.9695) && (lon < 113.971);
+       
         const ktown = (lat > 22.279) && (lat < 22.285) && (lon > 114.125) && (lon < 114.130);
         const central = (lat > 22.281) && (lat < 22.290) && (lon > 114.156) && (lon < 114.159);
         const causeway = (lat > 22.270) && (lat < 22.283) && (lon > 114.180) && (lon < 114.190);
         const westKowloon = (lat > 22.298) && (lat < 22.308) && (lon > 114.154) && (lon < 114.165);
-        const tuenMun = (lat > 22.37) && (lat < 22.41) && (lon > 113.96) && (lon < 113.985);
+        const tsimShaTsui = (lat > 22.292) && (lat < 22.303) && (lon > 114.165) && (lon < 114.177);
 
 
         // 22.273,114.121 top left
@@ -144,6 +146,15 @@ function smartCards(){
         if (home) {
             district = "Baguio Villa"
         }
+
+        if (tuenMun) {
+            district = "Tuen Mun"
+        }
+        if (work) {
+            district = "Office"
+        }
+
+
         if (ktown) {
             district = "Kennedy Town"
         }
@@ -156,13 +167,11 @@ function smartCards(){
         if (westKowloon) {
             district = "West Kowloon"
         }
+        if (tsimShaTsui) {
+            district = "Tsim Sha Tsui"
+        }
         
-        if (tuenMun) {
-            district = "Tuen Mun"
-        }
-        if (work) {
-            district = "Office"
-        }
+        
 
 
 
