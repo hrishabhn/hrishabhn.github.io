3299fd6f265c8b9f5cf1666aa93dd171e3a7a624
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2021-07-14 01:07:28 +0800

    Create load.js

---
+++ script/load.js
@@ -0,0 +1,20 @@
+function mainPopulate(){
+    calendarDate()
+    // getLocation()
+    // test()
+    smartCards()
+    apps()
+    media()
+    statusBar(1000,4000)
+    widgets()
+    kmbWidget("961","490F1A302D8C32FC",1)
+    kmbWidget("960","AD97EA183A25102C",2)
+    cityBusWidget("CTB","43m","002665",3)
+    cityBusWidget("NWFB","970x","002236",4)
+
+    // busETAPopulate(961)
+}
+
+function test() {
+    console.log(lat)
+}
