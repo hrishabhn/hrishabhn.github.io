9d5b18f822ab0bd634f9b9e4839ff54c21f7f943
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2021-07-14 01:07:22 +0800

    Create calendar.js

---
+++ script/calendar.js
@@ -0,0 +1,23 @@
+function calendarDate() {
+    fullDate = new Date();
+
+    const weekDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    const months = ["JAN","FEB","MAR","APR","MAY","JUN","July"]
+
+    hourNow = fullDate.getHours();
+
+    weekdayNow = fullDate.getDay()
+    weekdayNow = weekDays[weekdayNow]
+    
+    monthNow = fullDate.getMonth()
+    monthNow = months[monthNow]
+    
+    dateNow = fullDate.getDate();
+    // console.log(dateNow)
+    // calendarLog()
+}
+
+// function calendarLog() {
+//     console.log(hourNow)
+//     console.log(weekdayNow)
+// }
