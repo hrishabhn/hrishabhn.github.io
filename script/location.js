1170fb899dd586e7db3e1a8c79b2d7a73ae7c4e6
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2021-07-14 01:07:33 +0800

    Create location.js

---
+++ script/location.js
@@ -0,0 +1,18 @@
+// var lat = 0;
+// var lon = 0;
+
+// function getLocation() {
+
+
+
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition((position) => {
+//             lat = position.coords.latitude;
+//             lon = position.coords.longitude;
+
+//         })
+//         // console.log(lon)
+
+//     }
+
+// }
