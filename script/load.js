9cfe1eeac9a03fa9cf4af33be64b09fa3e624e20
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2021-07-14 09:14:38 +0800

    Update load.js

--- script/load.js
+++ script/load.js
@@ -7,13 +7,18 @@ function mainPopulate(){
     media()
     statusBar(1000,4000)
     widgets()
-    kmbWidget("961","490F1A302D8C32FC",1)
-    kmbWidget("960","AD97EA183A25102C",2)
-    cityBusWidget("CTB","43m","002665",3)
-    cityBusWidget("NWFB","970x","002236",4)
-
+    buses()
     // busETAPopulate(961)
 }
+
+function mainRefresh() {
+    smartCards()
+    widgets()
+    buses()
+    statusBar(0,1500)
+    console.log("refreshed")
+}
+
 
 function test() {
     console.log(lat)
