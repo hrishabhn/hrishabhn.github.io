0cc0f239bc7ccdda39fec0f199d0356098561141
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2021-07-16 09:44:16 +0800

    Update load.js

--- script/load.js
+++ script/load.js
@@ -3,6 +3,7 @@ function mainPopulate(){
     getUserLocale()
 
     todayWidget()
+    // test()
 
 
     apps()
@@ -35,5 +36,21 @@ function mainRefresh() {
 
 
 // function test() {
-//     console.log(lat)
+//     document.getElementById('file').onchange = function(){
+
+//         var file = this.files[0];
+      
+//         var reader = new FileReader();
+//         reader.onload = function(progressEvent){
+//           // Entire file
+//           console.log(this.result);
+      
+//           // By lines
+//           var lines = this.result.split('\n');
+//           for(var line = 0; line < lines.length; line++){
+//             console.log(lines[line]);
+//           }
+//         };
+//         reader.readAsText(file);
+//     };
 // }
