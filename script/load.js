3deeb24c80c9b3608e82644f874a9bd8ed618cf1
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2021-07-17 15:13:21 +0800

    Update load.js

--- script/load.js
+++ script/load.js
@@ -3,25 +3,17 @@ function mainPopulate(){
     getUserLocale()
 
     todayWidget()
-    // test()
-
 
     apps()
     media()
 
     statusBar(1000,4000)
-    // lights()
 }
 
 function locationBasedLoad(userLocale) {
     document.getElementById("district").innerHTML = userLocale;
-
     smartCards(userLocale)
-    // console.log("bruhhh")
-
     buses(userLocale)
-    // console.log("bruhhh")
-
 }
 
 function mainRefresh() {
@@ -29,28 +21,6 @@ function mainRefresh() {
     getUserLocale()
     smartCards()
     todayWidget()
-    // buses()
     statusBar(0,1500)
     // console.log("refreshed")
 }
-
-
-// function test() {
-//     document.getElementById('file').onchange = function(){
-
-//         var file = this.files[0];
-      
-//         var reader = new FileReader();
-//         reader.onload = function(progressEvent){
-//           // Entire file
-//           console.log(this.result);
-      
-//           // By lines
-//           var lines = this.result.split('\n');
-//           for(var line = 0; line < lines.length; line++){
-//             console.log(lines[line]);
-//           }
-//         };
-//         reader.readAsText(file);
-//     };
-// }
