658cb7c8611b3859e72ade8af4a654581793522c
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2021-07-18 22:15:54 +0800

    Update bus.js

--- script/bus.js
+++ script/bus.js
@@ -484,13 +484,13 @@ var busBlur = document.getElementById("bus-blur");
 var bgBlur = document.getElementById("background-blur");
 
 function popupBusShow(){
-//   bgBlur.className = "background-blur background-blur-show";
+  bgBlur.className = "background-blur background-blur-show";
   busBlur.className = "bus-blur bus-blur-show";
   busModal.className = "modal bus-popup-open";
 }
 
 function popupBusHide(){
-//   bgBlur.className = "background-blur background-blur-hide";
+  bgBlur.className = "background-blur background-blur-hide";
   busBlur.className = "bus-blur bus-blur-hide";
   busModal.className = "modal bus-popup-closed";
 }
