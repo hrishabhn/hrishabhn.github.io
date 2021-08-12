db8c50a2d577937431d043d8d7b05c9380d1ff36
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2021-08-12 23:38:29 +0800

    Update pages.js

--- script/pages.js
+++ script/pages.js
@@ -1,85 +1,124 @@
-var main1 = document.getElementById("main-1");
-var testing = document.getElementById("main-2");
+// var main1 = document.getElementById("main-1");
+// var testing = document.getElementById("main-2");
 
+var mainCurrent = 1;
 
-function mainPage1(){
-  document.getElementById("main-1").className = "main-new";
-  document.getElementById("main-2").className = "hidden-always";
-  document.getElementById("main-3").className = "hidden-always";
-  document.getElementById("main-4").className = "hidden-always";
-  document.getElementById("main-5").className = "hidden-always";
+function mainPage(n) {
+  var oldMain = document.getElementById("main-" + mainCurrent)
+  var oldMenu = document.getElementById("menu-" + mainCurrent)
+  oldMain.classList.add("hidden-always")
+  oldMenu.classList.remove("selected")
 
-  document.getElementById("menu-1").className = "menu-item selected";
-  document.getElementById("menu-2").className = "menu-item";
-  document.getElementById("menu-3").className = "menu-item";
-  document.getElementById("menu-4").className = "menu-item";
-  document.getElementById("menu-5").className = "menu-item";
+  mainCurrent = n;
+  var newMain = document.getElementById("main-" + mainCurrent)
+  var newMenu = document.getElementById("menu-" + mainCurrent)
+  newMain.classList.remove("hidden-always")
+  newMenu.classList.add("selected")
 
-  document.getElementById("title-page").innerHTML = "Home";
+  pageLoads(n)
 }
 
-function mainPage2(){
-  document.getElementById("main-1").className = "hidden-always";
-  document.getElementById("main-2").className = "main-new";
-  document.getElementById("main-3").className = "hidden-always";
-  document.getElementById("main-4").className = "hidden-always";
-  document.getElementById("main-5").className = "hidden-always";
+function pageLoads(n) {
+  var title = document.getElementById("title-page")
 
-  document.getElementById("menu-1").className = "menu-item";
-  document.getElementById("menu-2").className = "menu-item selected";
-  document.getElementById("menu-3").className = "menu-item";
-  document.getElementById("menu-4").className = "menu-item";
-  document.getElementById("menu-5").className = "menu-item";
-
-  document.getElementById("title-page").innerHTML = "Bookmarks";
+  switch (n) {
+    case 1:
+      title.innerHTML = "Home"
+      break
+    case 2:
+      title.innerHTML = "Bookmarks"
+      break
+    case 3:
+      title.innerHTML = "Apps"
+      break
+    case 4:
+      title.innerHTML = "School"
+      break
+    case 5:
+      title.innerHTML = "Media"
+      tvLoad(1)
+      break
+  }
 }
 
-function mainPage3(){
-  document.getElementById("main-1").className = "hidden-always";
-  document.getElementById("main-2").className = "hidden-always";
-  document.getElementById("main-3").className = "main-new";
-  document.getElementById("main-4").className = "hidden-always";
-  document.getElementById("main-5").className = "hidden-always";
+// function mainPage1(){
+//   document.getElementById("main-1").className = "main-new";
+//   document.getElementById("main-2").className = "hidden-always";
+//   document.getElementById("main-3").className = "hidden-always";
+//   document.getElementById("main-4").className = "hidden-always";
+//   document.getElementById("main-5").className = "hidden-always";
 
-  document.getElementById("menu-1").className = "menu-item";
-  document.getElementById("menu-2").className = "menu-item";
-  document.getElementById("menu-3").className = "menu-item selected";
-  document.getElementById("menu-4").className = "menu-item";
-  document.getElementById("menu-5").className = "menu-item";
+//   document.getElementById("menu-1").className = "menu-item selected";
+//   document.getElementById("menu-2").className = "menu-item";
+//   document.getElementById("menu-3").className = "menu-item";
+//   document.getElementById("menu-4").className = "menu-item";
+//   document.getElementById("menu-5").className = "menu-item";
 
-  document.getElementById("title-page").innerHTML = "Apps";
-}
+//   document.getElementById("title-page").innerHTML = "Home";
+// }
 
-function mainPage4(){
-  document.getElementById("main-1").className = "hidden-always";
-  document.getElementById("main-2").className = "hidden-always";
-  document.getElementById("main-3").className = "hidden-always";
-  document.getElementById("main-4").className = "main-new";
-  document.getElementById("main-5").className = "hidden-always";
+// function mainPage2(){
+//   document.getElementById("main-1").className = "hidden-always";
+//   document.getElementById("main-2").className = "main-new";
+//   document.getElementById("main-3").className = "hidden-always";
+//   document.getElementById("main-4").className = "hidden-always";
+//   document.getElementById("main-5").className = "hidden-always";
 
-  document.getElementById("menu-1").className = "menu-item";
-  document.getElementById("menu-2").className = "menu-item";
-  document.getElementById("menu-3").className = "menu-item";
-  document.getElementById("menu-4").className = "menu-item selected";
-  document.getElementById("menu-5").className = "menu-item";
+//   document.getElementById("menu-1").className = "menu-item";
+//   document.getElementById("menu-2").className = "menu-item selected";
+//   document.getElementById("menu-3").className = "menu-item";
+//   document.getElementById("menu-4").className = "menu-item";
+//   document.getElementById("menu-5").className = "menu-item";
 
-  document.getElementById("title-page").innerHTML = "School";
-}
+//   document.getElementById("title-page").innerHTML = "Bookmarks";
+// }
 
-function mainPage5(){
-  document.getElementById("main-1").className = "hidden-always";
-  document.getElementById("main-2").className = "hidden-always";
-  document.getElementById("main-3").className = "hidden-always";
-  document.getElementById("main-4").className = "hidden-always";
-  document.getElementById("main-5").className = "main-new";
+// function mainPage3(){
+//   document.getElementById("main-1").className = "hidden-always";
+//   document.getElementById("main-2").className = "hidden-always";
+//   document.getElementById("main-3").className = "main-new";
+//   document.getElementById("main-4").className = "hidden-always";
+//   document.getElementById("main-5").className = "hidden-always";
 
-  document.getElementById("menu-1").className = "menu-item";
-  document.getElementById("menu-2").className = "menu-item";
-  document.getElementById("menu-3").className = "menu-item";
-  document.getElementById("menu-4").className = "menu-item";
-  document.getElementById("menu-5").className = "menu-item selected";
+//   document.getElementById("menu-1").className = "menu-item";
+//   document.getElementById("menu-2").className = "menu-item";
+//   document.getElementById("menu-3").className = "menu-item selected";
+//   document.getElementById("menu-4").className = "menu-item";
+//   document.getElementById("menu-5").className = "menu-item";
 
-  document.getElementById("title-page").innerHTML = "Media";
+//   document.getElementById("title-page").innerHTML = "Apps";
+// }
+
+// function mainPage4(){
+//   document.getElementById("main-1").className = "hidden-always";
+//   document.getElementById("main-2").className = "hidden-always";
+//   document.getElementById("main-3").className = "hidden-always";
+//   document.getElementById("main-4").className = "main-new";
+//   document.getElementById("main-5").className = "hidden-always";
+
+//   document.getElementById("menu-1").className = "menu-item";
+//   document.getElementById("menu-2").className = "menu-item";
+//   document.getElementById("menu-3").className = "menu-item";
+//   document.getElementById("menu-4").className = "menu-item selected";
+//   document.getElementById("menu-5").className = "menu-item";
+
+//   document.getElementById("title-page").innerHTML = "School";
+// }
+
+// function mainPage5(){
+//   document.getElementById("main-1").className = "hidden-always";
+//   document.getElementById("main-2").className = "hidden-always";
+//   document.getElementById("main-3").className = "hidden-always";
+//   document.getElementById("main-4").className = "hidden-always";
+//   document.getElementById("main-5").className = "main-new";
+
+//   document.getElementById("menu-1").className = "menu-item";
+//   document.getElementById("menu-2").className = "menu-item";
+//   document.getElementById("menu-3").className = "menu-item";
+//   document.getElementById("menu-4").className = "menu-item";
+//   document.getElementById("menu-5").className = "menu-item selected";
+
+//   document.getElementById("title-page").innerHTML = "Media";
   
-  tvLoad(1)
-}
+//   tvLoad(1)
+// }
