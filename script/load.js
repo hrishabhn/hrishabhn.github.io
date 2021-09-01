284b18826019a103ac666e436e1e7636a88b58aa
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2021-09-01 12:59:01 -0400

    Update load.js

--- script/load.js
+++ script/load.js
@@ -29,6 +29,17 @@ function newPopulate(){
     // flightPull()
     // showMediaPopup()
 }
+function openFullscreen() {
+    const elem = document.documentElement
+
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+}
 
 function locationBasedLoad(userLocale) {
     storeLocale(userLocale)
