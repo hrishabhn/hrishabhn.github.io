73dc4541864730eff53b184eabf0eb155383ab86
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2021-09-03 02:04:23 -0400

    Update pages.js

--- script/pages.js
+++ script/pages.js
@@ -54,6 +54,10 @@ const menuData = [
     icon: '<svg class="icon" data-bbox="15.49 8.193 132.882 147.475" viewBox="0 0 163.861 163.861" xmlns="http://www.w3.org/2000/svg" data-type="shape"><g><path d="M39.564 11.445C26.27 3.818 15.49 10.065 15.49 25.388v113.074c0 15.338 10.78 21.577 24.075 13.958l98.832-56.68c13.3-7.629 13.3-19.99 0-27.617L39.564 11.445z" xmlns="http://www.w3.org/2000/svg" /></g></svg>',
     color: 'netflix',
   },
+  {
+    type: 'title',
+    name: 'TEST',
+  },
 ]
 
 function menuSwitchApps() {
