3d2c4eeea7d04c903e6aa4b751ccb31329d7904b
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2021-10-17 03:21:53 -0400

    Update elements.js

--- newdash/script/elements.js
+++ newdash/script/elements.js
@@ -80,7 +80,17 @@ function searchElement(contentItem) {
     element.href = contentItem.link
     element.target = '_blank'
     element.classList = 'layer-1 button-text card-shadow clickable'
-    element.innerHTML = contentItem.engine
+
+    var icon = document.createElement('div')
+    icon.classList = 'icon'
+    icon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 56.966 56.966"><path d="M55.146 51.887L41.588 37.786A22.926 22.926 0 0046.984 23c0-12.682-10.318-23-23-23s-23 10.318-23 23 10.318 23 23 23c4.761 0 9.298-1.436 13.177-4.162l13.661 14.208c.571.593 1.339.92 2.162.92.779 0 1.518-.297 2.079-.837a3.004 3.004 0 00.083-4.242zM23.984 6c9.374 0 17 7.626 17 17s-7.626 17-17 17-17-7.626-17-17 7.626-17 17-17z"></path></svg>'
+    element.append(icon)
+
+    element.append(spacerElement(10))
+
+    var text = document.createElement('p')
+    text.innerHTML = contentItem.engine
+    element.append(text)
     // console.log(element)
 
     return element
