f2d6cfe108fd90dcaa6bc45df2892809fa0121f7
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2021-11-05 19:07:51 -0400

    2

--- script/load.js
+++ script/load.js
@@ -55,24 +55,3 @@ const baseElements = [
     }
 ]
 
-function behindPass() {
-    if (!checkPass()) {
-        getPass()
-
-    }
-}
-
-function checkPass() {
-    return (!!getCookie('verified'))
-}
-
-function getPass() {
-    var pass = prompt('Enter password:')
-
-    while (pass != 'pass') {
-        pass = prompt('Wrong password, try again:')
-    }
-
-    setCookie('verified',true,14)
-    console.log(getCookie('verified'))
-}
