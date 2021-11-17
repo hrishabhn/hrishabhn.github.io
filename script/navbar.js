f2977de2f47d6a0065b1452fed4369ff81e0bbf8
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2021-11-16 20:37:39 -0500

    ,

--- script/navbar.js
+++ script/navbar.js
@@ -36,7 +36,7 @@ function navbarLeft() {
     container.classList = 'side left'
 
     var search = document.createElement('a')
-    search.classList = 'search item clickable layer-2'
+    search.classList = 'basic item clickable layer-2'
     search.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 56.966 56.966"><path d="M55.146 51.887L41.588 37.786A22.926 22.926 0 0046.984 23c0-12.682-10.318-23-23-23s-23 10.318-23 23 10.318 23 23 23c4.761 0 9.298-1.436 13.177-4.162l13.661 14.208c.571.593 1.339.92 2.162.92.779 0 1.518-.297 2.079-.837a3.004 3.004 0 00.083-4.242zM23.984 6c9.374 0 17 7.626 17 17s-7.626 17-17 17-17-7.626-17-17 7.626-17 17-17z"></path></svg>'
     search.onclick = function() {
         spotlightShow()
@@ -108,13 +108,12 @@ function navbarRight() {
     weather.append(temp)
 
     container.append(weather)
-
     container.append(spacerElement(10))
 
     var focusContainer = document.createElement('div')
     
     var focus = document.createElement('a')
-    focus.classList = 'focus item clickable layer-2'
+    focus.classList = 'basic item clickable layer-2'
     focus.innerHTML = currentFocus().icon
     focus.onclick = function() { focusMenuToggle() }
 
@@ -123,8 +122,13 @@ function navbarRight() {
 
 
     container.append(focusContainer)
+    container.append(spacerElement(10))
 
-
+    var idea = document.createElement('a')
+    idea.classList = 'basic item clickable layer-2'
+    idea.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="4 4 40 40"><path d="M16.415 31.906 16.88 34h14.24l.465-2.094c.315-1.417.988-2.752 2.041-3.751C36.395 25.53 38 21.864 38 18c0-7.718-6.283-13.975-14-13.978-7.717.003-14 6.26-14 13.978 0 3.864 1.605 7.53 4.373 10.155 1.054.999 1.727 2.334 2.042 3.751z"/><path d="m17.547 37 .946 4.259C18.849 42.861 20.27 44 21.91 44h4.18c1.64 0 3.061-1.139 3.417-2.741L30.453 37H17.547z"/></svg>'
+    idea.href = 'https://www.notion.so/Inbox-90dd1c97df2d4bee8c605c27dfb5fb54'
+    container.append(idea)
     container.append(spacerElement(10))
 
     var user = document.createElement('a')
