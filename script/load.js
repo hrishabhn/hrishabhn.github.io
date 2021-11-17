ee1910c8be6a133815f2f1a8459a1996a284c8a4
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2021-11-16 20:23:17 -0500

    Update load.js

--- script/load.js
+++ script/load.js
@@ -7,7 +7,7 @@ function loadApp() {
     widgetPopulate()
     openFirstPage(0)
     // openModal(tripCard(0))
-    // openModal(flightDetail(flightData[0]))
+    // openModal(flightDetail(flightData[0],'flight'))
     // tvPopupShow(0,0)
     // testTV()
     calTest()
