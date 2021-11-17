48374c1b1f94b659e4f11dcfb7dbd58fabc18be1
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2021-11-17 02:43:34 -0500

    m

--- script/assistant.js
+++ script/assistant.js
@@ -1,9 +1,73 @@
+function assistantTop() {
+    let container = document.createElement('div')
+    container.classList = 'siri-top layer-1'
+    // container.innerHTML = 'nice'
+
+    let header = document.createElement('div')
+    header.classList = 'header'
+    
+    let title1 = document.createElement('p1')
+    title1.innerHTML = 'Your focus:'
+    
+    let title2 = document.createElement('p2')
+    title2.innerHTML = currentFocus().name
+
+    header.append(title1)
+    header.append(title2)
+    container.append(header)
+
+    // apps
+    let tray = document.createElement('div')
+    tray.classList = 'suggest-tray'
+
+    for (let i = 0; i < currentFocus().apps.length; i++) {
+        let appCont = document.createElement('div')
+        appCont.classList = 'vstack'
+    
+        let appIcon = document.createElement('a')
+        appIcon.classList = `icon ${currentFocus().apps[i].background} clickable`
+        appIcon.href = currentFocus().apps[i].link
+        appIcon.innerHTML = currentFocus().apps[i].icon
+        appCont.append(appIcon)
+    
+        let appTextbox = document.createElement('div')
+        appTextbox.style.setProperty('width','100%')
+
+        let appText = document.createElement('p')
+        appText.classList = 'app-text'
+        appText.innerHTML = currentFocus().apps[i].name
+        appTextbox.append(appText)
+        appCont.append(appTextbox)
+    
+        tray.append(appCont)
+
+        if (i < currentFocus().apps.length - 1) {
+            tray.append(growElement())
+        }
+    }
+
+
+    container.append(tray)
+
+    container.append(tray)
+    
+    return container
+}
+
 function assistantContainer() {
     let container = document.createElement('div')
     container.classList = 'siri-container layer-1'
     container.append(assistantInputElement())
     container.append(assistantResultElement())
     return container
+}
+
+function assistantInputClear() {
+    var input = document.getElementById('siri-search')
+    console.log(input)
+    input.value = ''
+    input.focus()
+    siriSearch()
 }
 
 function assistantInputElement() {
@@ -17,6 +81,12 @@ function assistantInputElement() {
     input.id = 'siri-search'
     input.onkeyup = function(e) { siriSearch(e) }
     container.append(input)
+
+    var clear = document.createElement('div')
+    clear.classList = 'clear layer-1-trans clickable'
+    clear.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="m14.8 16.2-7-7c-.4-.4-.4-1 0-1.4.4-.4 1-.4 1.4 0l7 7c.4.4.4 1 0 1.4-.4.4-1 .4-1.4 0z"></path><path d="m7.8 14.8 7-7c.4-.4 1-.4 1.4 0 .4.4.4 1 0 1.4l-7 7c-.4.4-1 .4-1.4 0-.4-.4-.4-1 0-1.4z"></path></svg>'
+    clear.onclick = function() { assistantInputClear() }
+    container.append(clear)
 
     return container
 }
@@ -32,9 +102,9 @@ function siriSearch(e) {
     const query = input.value
     const resultElement = document.getElementById('siri-result')
     
-    console.log(resultElement)
+    // console.log(resultElement)
     const results = globalSearchResults(query)
-    console.log(results)
+    // console.log(results)
     
     // console.log(e)
 
