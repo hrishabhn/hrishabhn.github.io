0943a85e28de8917b9cd2bfa263aa5bc60795dc4
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2022-02-21 21:09:47 +0100

    Update budgetData.js

--- script/budget/budgetData.js
+++ script/budget/budgetData.js
@@ -1,3 +1,5 @@
 let budgetData = [
     { "amount": 1.15, "name": "Coffee", "category": "Coffee", "date": "2022-02-21T13:56:00+01:00" },
+    { "amount": 15.7, "name": "Ramen", "category": "Grocery", "date": "2022-02-21T20:07:10+01:00" },
+    { "amount": 11.08, "name": "Five Guys", "category": "Food", "date": "2022-02-21T20:07:43+01:00" },
 ]
