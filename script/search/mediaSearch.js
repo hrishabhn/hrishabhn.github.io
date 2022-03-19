bdb2074395b383379d8e7e7e6082e1871e6c56b1
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2022-03-19 21:50:50 +0100

    Update mediaSearch.js

--- script/search/mediaSearch.js
+++ script/search/mediaSearch.js
@@ -256,7 +256,8 @@ function searchMoviesRowBig(results, title) {
 
 function searchActors(q) {
     let results = []
-    for (const tray of movieData) for (const movie of tray) if (movie.cast) for (const actor of movie.cast) if (actor.actor.toUpperCase().includes(q)) if (!results.includes(actor.actor)) results.push(actor.actor)
+    for (const tray of movieData) for (const movie of tray) if (movie.cast)
+        for (const actor of movie.cast) if (actor.actor.toUpperCase().includes(q)) if (!results.includes(actor.actor)) results.push(actor.actor)
     return results
 }
 
