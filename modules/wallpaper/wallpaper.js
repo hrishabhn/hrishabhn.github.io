722ec55ffa0da25c0a3f1ae37d933926e61d1aef
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2022-03-22 22:50:34 +0100

    a

--- modules/wallpaper/wallpaper.js
+++ modules/wallpaper/wallpaper.js
@@ -1,8 +1,10 @@
 function setWallpaper() {
-    const elem = document.getElementById('wallpaper')
+    if (!isDND()) {
+        const elem = document.getElementById('wallpaper')
+        elem.style.setProperty('--wp-light', 'url(./images/paint-light.jpg)')
+        elem.style.setProperty('--wp-dark', 'url(./images/paint-dark.jpg)')
+    }
     // elem.style.setProperty('--wp-light', 'url(../modules/wallpaper/images/tropical.jpg)')
-    elem.style.setProperty('--wp-light', 'url(./images/paint-light.jpg)')
     // elem.style.setProperty('--wp-dark', 'url(../modules/wallpaper/images/dark-mountain.jpg)')
-    elem.style.setProperty('--wp-dark', 'url(./images/paint-dark.jpg)')
     // elem.style.setProperty('--wp-dark', 'url(./images/space.jpg)')
 }
