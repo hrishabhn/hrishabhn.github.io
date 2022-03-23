c261deb4dfbef95d8c203d6ac0cede598db461fe
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2022-03-23 18:21:54 +0100

    Update modal.js

--- modules/modal/modal.js
+++ modules/modal/modal.js
@@ -1,5 +1,30 @@
 const modal = {
+    add: function (card) {
+        let layer = document.createElement('div')
+        layer.classList = 'layer'
+        layer.innerHTML = '<a class="close"></a>'
 
+        let close = document.createElement('a')
+        close.classList = 'close'
+        close.onclick = function () { removeModalLayer() }
+
+        layer.append(close)
+        layer.append(card)
+
+        layerModal.append(layer)
+        if (modalIndex == 0) {
+            layerModal.classList.remove('hide')
+        }
+
+        modalIndex++
+    }
+    remove: function () {
+        layerModal.lastChild.remove()
+        modalIndex--
+        if (modalIndex == 0) {
+            layerModal.classList.add('hide')
+        }
+    }
 }
 
 const layerModal = document.getElementById('layer-modal')
