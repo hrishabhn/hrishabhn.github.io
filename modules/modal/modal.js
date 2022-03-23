019fbd7dff661e8857fcffd34dfa6445b9769693
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2022-03-23 18:20:14 +0100

    a

---
+++ modules/modal/modal.js
@@ -0,0 +1,39 @@
+const modal = {
+
+}
+
+const layerModal = document.getElementById('layer-modal')
+
+function modalTest() {
+    addModalLayer(flightDetailCard('mad-lhr'))
+}
+
+let modalIndex = 0
+
+function addModalLayer(card) {
+    let layer = document.createElement('div')
+    layer.classList = 'layer'
+    layer.innerHTML = '<a class="close"></a>'
+    
+    let close = document.createElement('a')
+    close.classList = 'close'
+    close.onclick = function() { removeModalLayer() }
+
+    layer.append(close)
+    layer.append(card)
+
+    layerModal.append(layer)
+    if (modalIndex == 0) {
+        layerModal.classList.remove('hide')
+    }
+
+    modalIndex++
+}
+
+function removeModalLayer() {
+    layerModal.lastChild.remove()
+    modalIndex--
+    if (modalIndex == 0) {
+        layerModal.classList.add('hide')
+    }
+}
