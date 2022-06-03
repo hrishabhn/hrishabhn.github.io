12c13244c092d315baf58075a2048f00933b1fdf
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2022-06-03 20:26:24 +0200

    a

--- calendar/tasks_all.js
+++ calendar/tasks_all.js
@@ -1,5 +1,10 @@
 let tasks_all = [
+	'AirBNB June 30/Jul 1',
+	'Apartment Barcelona',
+	'Thermo University of Wyoming',
+	'Train Ticket MAD BCN',
 	'English Test for Wyoming',
+	'Audible',
 	'Granite Safety Deposit',
 	'Start date',
 ]
--- shortcuts/budgetData.js
+++ shortcuts/budgetData.js
@@ -211,4 +211,5 @@ let budgetData = [
 
 
 
+
 ]
