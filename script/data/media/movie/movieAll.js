6f3eeb653bfad7673c092515ee4204854165e06e
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2022-06-04 17:28:19 +0200

    Update movieAll.js

--- script/data/media/movie/movieAll.js
+++ script/data/media/movie/movieAll.js
@@ -3545,11 +3545,12 @@ const allMovies = {
     'always': {
         name: "It's Always Sunny in Philadelphia",
         id: 'always',
-        link: 'https://www.disneyplus.com/series/its-always-sunny-in-philadelphia/67id28oN9H8P',
+        // link: 'https://www.disneyplus.com/series/its-always-sunny-in-philadelphia/67id28oN9H8P',
+        link: 'https://www.hulu.com/series/its-always-sunny-in-philadelphia-2171423f-3326-4dfa-b193-b40494e60109',
         info: {
-            service: 'disney',
+            service: 'hulu',
             studio: 'fx',
-            location: 'Disney+',
+            location: 'Hulu',
             // date: 'Dec 23, 2021 03:00:00',
             // weekday: 'Thursday',
             seasons: 15,
