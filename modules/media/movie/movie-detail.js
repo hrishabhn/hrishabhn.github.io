5290adfc1e57618ba5ca04e2cacb7880b5c3da12
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2022-06-27 23:27:08 +0530

    Update movie-detail.js

--- modules/media/movie/movie-detail.js
+++ modules/media/movie/movie-detail.js
@@ -18,103 +18,103 @@ function hideTVDetail() {
 }
 
 const tvDetail = function(key) {
-    const movie = movieDict[key]
+        const movie = movieDict[key]
 
-    let card = document.createElement('div')
-    card.classList = 'tv-detail-card'
+        let card = document.createElement('div')
+        card.classList = 'tv-detail-card'
 
-    card.append(elems.bg(movie.style.hex))
+        card.append(elems.bg(movie.style.hex))
 
-    // desktop thumb
-    let wide = document.createElement('div')
-    wide.classList = 'fill-width wide-thumb'
+        // desktop thumb
+        let wide = document.createElement('div')
+        wide.classList = 'fill-width wide-thumb'
 
-    wide.append(elems.thumb(`./media-image/TV/background/${movie.id}.${movie.style.poster.wide.type}`))
-    wide.append(elems.grad())
-    card.append(wide)
+        wide.append(elems.thumb(`./media-image/TV/background/${movie.id}.${movie.style.poster.wide.type}`))
+        wide.append(elems.grad())
+        card.append(wide)
 
-    // mobile thumb
-    if (movie.style.poster.mobile.type) {
-        let tall = document.createElement('div')
-        tall.classList = 'fill-width tall-thumb only-mobile'
+        // mobile thumb
+        if (movie.style.poster.mobile.type) {
+            let tall = document.createElement('div')
+            tall.classList = 'fill-width tall-thumb only-mobile'
 
-        let thumb = elems.thumb(`./media-image/TV/mobile/${movie.id}.${movie.style.poster.mobile.type}`)
-        thumb.style.setProperty('aspect-ratio', movie.style.poster.mobile.size)
+            let thumb = elems.thumb(`./media-image/TV/mobile/${movie.id}.${movie.style.poster.mobile.type}`)
+            thumb.style.setProperty('aspect-ratio', movie.style.poster.mobile.size)
 
-        let grad = elems.grad()
-        grad.style.setProperty('background-image', `linear-gradient(#00000000,#${movie.style.hex})`)
-        thumb.append(grad)
+            let grad = elems.grad()
+            grad.style.setProperty('background-image', `linear-gradient(#00000000,#${movie.style.hex})`)
+            thumb.append(grad)
 
-        let title = movieCardTitle(key)
-        title.classList.add('only-mobile')
+            let title = movieCardTitle(key)
+            title.classList.add('only-mobile')
 
-        tall.append(thumb)
-        card.append(tall)
-        card.append(title)
+            tall.append(thumb)
+            card.append(tall)
+            card.append(title)
 
-        wide.classList.add('hidden-mobile')
-    } else {
-        card.style.setProperty('margin', '0 50px')
-    }
-
-    // title, desc, play button
-    let info = elems.textbox(movie.name, movie.desc.full)
-    info.firstChild.classList.add('hidden-mobile')
-    info.classList = 'info'
-
-    let play = elems.a(iconData.play, movie.info.location)
-    play.classList = 'play clickable'
-    play.onclick = function () { movie.play() }
-
-    info.firstChild.after(play)
-
-    // summary
-    if (movie.info.summary) {
-        let sum = elems.p(movie.info.summary)
-        sum.classList = 'summary'
-        info.append(sum)
-    }
-
-    // apps
-    info.append(movieDetail.app.tray(movie))
-    card.append(info)
-    card.append(elems.grow())
-
-    if (movie.cast || movie.info.tags) {
-        let more = document.createElement('div')
-        more.classList = 'extra layer-0 primary-fg'
-
-        // cast
-        if (movie.cast) {
-            more.append(actors.row(movie.cast, 'Cast'))
-            more.append(elems.hline())
+            wide.classList.add('hidden-mobile')
+        } else {
+            card.style.setProperty('margin', '0 50px')
         }
 
-        // suggest
-        if (movie.info.tags) for (const mainTag of movie.info.tags) {
-            let results = []
-            for (const tray of movieData) for (item of tray)
-                if (item.info.tags) for (const tag of item.info.tags)
-                    if ((tag == mainTag) && (item.id != key)) results.push(item.id)
-            if (results.length) more.append(mediaElems.movie.row(results, `More in ${movies.tags[mainTag]}`, 'small'))
-            more.append(elems.hline())
+        // title, desc, play button
+        let info = elems.textbox(movie.name, movie.desc.full)
+        info.firstChild.classList.add('hidden-mobile')
+        info.classList = 'info'
+
+        let play = elems.a(iconData.play, movie.info.location)
+        play.classList = 'play clickable'
+        play.onclick = function () { movie.play() }
+
+        info.firstChild.after(play)
+
+        // summary
+        if (movie.info.summary) {
+            let sum = elems.p(movie.info.summary)
+            sum.classList = 'summary'
+            info.append(sum)
         }
 
-        more.lastChild.remove()
+        // apps
+        info.append(movieDetail.app.tray(movie))
+        card.append(info)
+        card.append(elems.grow())
 
-        card.append(more)
-    }
+        if (movie.cast || movie.info.tags) {
+            let more = document.createElement('div')
+            more.classList = 'extra layer-0 primary-fg'
+
+            // cast
+            if (movie.cast) {
+                more.append(actors.row(movie.cast, 'Cast'))
+                more.append(elems.hline())
+            }
+
+            // suggest
+            if (movie.info.tags) for (const mainTag of movie.info.tags) {
+                let results = []
+                for (const tray of movieData) for (item of tray)
+                    if (item.info.tags) for (const tag of item.info.tags)
+                        if ((tag == mainTag) && (item.id != key)) results.push(item.id)
+                if (results.length) more.append(mediaElems.movie.row(results, `More in ${movies.tags[mainTag]}`, 'small'))
+                more.append(elems.hline())
+            }
+
+            more.lastChild.remove()
+
+            card.append(more)
+        }
 
 
 
-    let close = elems.a(iconData.close, null)
-    close.classList = 'close-tv-modal clickable-o'
-    close.onclick = function () {
-        hideTVDetail()
-    }
-    card.append(close)
+        let close = elems.a(iconData.close, null)
+        close.classList = 'close-tv-modal clickable-o'
+        close.onclick = function () {
+            hideTVDetail()
+        }
+        card.append(close)
 
-    return card
+        return card
 }
 
 function movieApps(movie) {
