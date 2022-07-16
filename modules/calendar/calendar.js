9dbeb242cacbf6bb8d4c405ecbaeb2b859b4eb31
hrishabhn <84631552+hrishabhn@users.noreply.github.com>
2022-07-17 01:02:49 +0200

    A

--- modules/calendar/calendar.js
+++ modules/calendar/calendar.js
@@ -1,76 +1,4 @@
 const calendar = {
-    topCard: function () {
-        let card = calendar.splashCard()
-        card.classList.remove('splash-card')
-        card.classList.add('top-card')
-        return card
-    },
-    splashCard: function () {
-        // base card with header
-        const title = `${processDate.day.long(new Date())}, ${new Date().getDate()} ${processDate.month.long(new Date())}`
-        let card = leftbar.cardBase(title, [{ icon: SFSymbols.calendar }])
-        card.id = 'cal'
-
-        let body = document.createElement('div')
-        body.classList = 'body'
-
-        if (calEvents.today.length) {
-            for (const event of calEvents.today) {
-                body.append(calendar.eventCard(event))
-                body.append(elems.spacer(5))
-            }
-        }
-
-        if (calEvents.tomor.length) {
-            body.append(elems.subtitle('TOMORROW'))
-            for (const event of calEvents.tomor) {
-                body.append(calendar.eventCard(event))
-                body.append(elems.spacer(5))
-            }
-        }
-
-        if (!calEvents.today.length && !calEvents.tomor.length) body.append(elems.subtitle('No more events today or tomorrow.'))
-
-        card.append(body)
-        return card
-    },
-    eventCard: function (event) {
-        let eventCard = document.createElement('a')
-        eventCard.classList = 'event clickable-o'
-        eventCard.style.setProperty('--col', `#${event.hex}`)
-        if (event.hasStarted) { eventCard.classList.add('started') }
-
-        let colBlock = document.createElement('span')
-
-        let info = document.createElement('div')
-        info.classList = 'info'
-
-        // 
-        info.append(elems.bg(event.hex))
-        if (!event.allDay) {
-            info.append(elems.textbox(event.name, event.duration))
-        } else {
-            info.append(elems.textbox(event.name, null))
-        }
-
-        info.append(elems.grow())
-
-        if (event.icon) { info.append(elems.icon(event.icon)) }
-
-        if (event.link) {
-            eventCard.href = event.link
-            eventCard.style.setProperty('cursor', 'pointer')
-        } else if (event.trigger) {
-            eventCard.onclick = function () { event.trigger() }
-            eventCard.style.setProperty('cursor', 'pointer')
-        }
-
-
-        eventCard.append(colBlock)
-        eventCard.append(info)
-
-        return eventCard
-    },
     resultCard: function () {
         const title = `${processDate.day.long(new Date())}, ${new Date().getDate()} ${processDate.month.long(new Date())}`
         let card = resultCard.base({
@@ -85,10 +13,16 @@ const calendar = {
         if (calEvents.today.length && calEvents.tomor.length) {
             if (calEvents.today.length) for (const event of calEvents.today) card.lastChild.append(calendar.resultCardItem(event))
             if (calEvents.tomor.length) {
-                card.lastChild.append(elems.subtitle('TOMORROW'))
+                let p = elems.p('TOMORROW')
+                p.classList = 'calendar-sub-title'
+                card.lastChild.append(p)
                 for (const event of calEvents.tomor) card.lastChild.append(calendar.resultCardItem(event))
             }
-        } else card.lastChild.append(elems.subtitle('No more events today or tomorrow.'))
+        } else {
+            let p = elems.p('No more events today or tomorrow.')
+            p.classList = 'calendar-sub-title'
+            card.lastChild.append(p)
+        }
 
         return card
     },
@@ -114,8 +48,6 @@ const calendar = {
 
         // icon
         // trigger + link
-
-
 
         return item
     },
